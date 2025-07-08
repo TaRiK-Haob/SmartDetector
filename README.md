b05e19dd2d223434d1431c6107642660187b5706
HaoB <1160280151@qq.com>
2025-07-08 15:57:33 +0800

    Added Resnet model

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # SmartDetector
 Reproduce of SmartDetector.
+
+---
+A roughly reproduce of [SmartDetector](https://ieeexplore.ieee.org/abstract/document/10964328), which consist of two parts:
+
+![Part 1: Word2Vec](image/image.png)
+
+![Part 2: Contrastive Learning](image/image2.png)
---
+++ models.py
@@ -0,0 +1,112 @@
+import torch
+
+# K -> Number of Pakcet
+# B -> dimension of embeddings
+K, B = 40, 100
+
+class FeatureEmbedding(torch.nn.Module):
+    def __init__(self, input_dim, output_dim):
+        super(FeatureEmbedding, self).__init__()
+        self.direction_embedding = torch.nn.Linear(input_dim, output_dim)
+        self.pktlen_embedding = torch.nn.Linear(input_dim, output_dim)
+        self.IAT_embedding = torch.nn.Linear(input_dim, output_dim)
+
+    def forward(self, x):
+        pass
+
+
+class BasicBlock(torch.nn.Module):
+    def __init__(self, in_channels, out_channels, stride=1, downsample=None):
+        super(BasicBlock, self).__init__()
+        self.conv1 = torch.nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn1 = torch.nn.BatchNorm2d(out_channels)
+        self.relu = torch.nn.ReLU(inplace=True)
+        self.conv2 = torch.nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn2 = torch.nn.BatchNorm2d(out_channels)
+        self.downsample = downsample
+        self.stride = stride
+
+    def forward(self, x):
+        identity = x
+
+        if self.downsample is not None:
+            identity = self.downsample(x)
+        
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+        out = self.conv2(out)
+        out = self.bn2(out)
+
+        out += identity
+        out = self.relu(out)
+        return out
+
+
+class ResNet(torch.nn.Module):
+    def __init__(self, block, layers, num_classes=1000):
+        super(ResNet, self).__init__()
+        self.in_channels = 64
+        self.conv1 = torch.nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False)
+        self.bn1 = torch.nn.BatchNorm2d(64)
+        self.relu = torch.nn.ReLU(inplace=True)
+        self.maxpool = torch.nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        self.layer1 = self._make_layer(block, 64, layers[0])
+        self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, layers[3], stride=2)
+        self.avgpool = torch.nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = torch.nn.Linear(512 * block.expansion, num_classes)
+
+    def _make_layer(self, block, out_channels, blocks, stride=1):
+        downsample = None
+        if stride != 1 or self.in_channels != out_channels:
+            downsample = torch.nn.Sequential(
+                torch.nn.Conv2d(self.in_channels, out_channels, kernel_size=1, stride=stride, bias=False),
+                torch.nn.BatchNorm2d(out_channels),
+            )
+        
+        layers = []
+        layers.append(block(self.in_channels, out_channels, stride, downsample))
+        self.in_channels = out_channels * block.expansion
+        for _ in range(1, blocks):
+            layers.append(block(out_channels, out_channels))
+        
+        return torch.nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+        
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+
+        x = self.avgpool(x)
+        x = torch.flatten(x, 1)
+        x = self.fc(x)
+        
+        return x
+    
+    
+def resnet50(num_classes=1000):
+    return ResNet(BasicBlock, [3, 4, 6, 3], num_classes)
+
+class SmartDetector(torch.nn.Module):
+    def __init__(self, input_dim, output_dim):
+        super(SmartDetector, self).__init__()
+        self.feature_embedding = FeatureEmbedding(input_dim, output_dim)
+        self.fc1 = torch.nn.Linear(output_dim * 3, 128)
+        self.fc2 = torch.nn.Linear(128, 64)
+        self.fc3 = torch.nn.Linear(64, 1)
+
+    def forward(self, x):
+        embedded_features = self.feature_embedding(x)
+        x = torch.cat(embedded_features, dim=1)
+        x = torch.relu(self.fc1(x))
+        x = torch.relu(self.fc2(x))
+        x = torch.sigmoid(self.fc3(x))
+        return x
